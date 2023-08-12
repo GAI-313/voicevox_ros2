e4dd7957ea5680b8037992f55869a76d74e7d172
Gai Nakatogawa <nakatogawagai@gmail.com>
2023-08-12 16:24:44 +0900

    パスが記述されない問題を修正

--- setup.bash
+++ setup.bash
@@ -8,5 +8,11 @@ wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.1/voicevox
 pip install voicevox_core-0.14.1+cpu-cp38-abi3-linux_x86_64.whl
 wget https://raw.githubusercontent.com/VOICEVOX/voicevox_core/406f6c41408836840b9a38489d0f670fb960f412/example/python/run.py
 
-echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$current_dir/voicevox_core\"" >> ~/.bashrc
-echo "export JTALK_PATH=\"\$LD_LIBRARY_PATH:$current_dir/voicevox_core/open_jtalk_dic_utf_8-1.11\"" >> ~/.bashrc
+search_string="#test_setting"
+if grep -q "$search_string" ~/.bashrc; then
+    true
+else
+    echo "#voicevox_ros2 setting" >> ~/.bashrc
+    echo "export LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH:$current_dir/voicevox_core\"" >> ~/.bashrc
+    echo "export JTALK_PATH=\"$current_dir/voicevox_core/open_jtalk_dic_utf_8-1.11\"" >> ~/.bashrc
+fi

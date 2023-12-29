522472723168abba48482ae915a9ad39a8c9fb0f
GAI <67356536+GAI-313@users.noreply.github.com>
2023-12-29 21:39:45 +0900

    VoiceVoxCore Update

--- voicevox_ros2_pkg_setup.sh
+++ voicevox_ros2_pkg_setup.sh
@@ -21,7 +21,8 @@ if [ -d "$current_dir/voicevox_core" ]; then
 else
     curl -sSfL https://raw.githubusercontent.com/VOICEVOX/voicevox_core/8cf307df4412dc0db0b03c6957b83b032770c31a/scripts/downloads/download.sh | bash -s
     cd "$current_dir/voicevox_core" || exit 1
-    wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.1/voicevox_core-0.14.1+cpu-cp38-abi3-linux_x86_64.whl
+    #wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.1/voicevox_core-0.14.1+cpu-cp38-abi3-linux_x86_64.whl
+    wget https://github.com/VOICEVOX/voicevox_core/releases/download/0.14.5/voicevox_core-0.14.5+cpu-cp38-abi3-linux_x86_64.whl
     pip install voicevox_core*.whl
     wget https://raw.githubusercontent.com/VOICEVOX/voicevox_core/406f6c41408836840b9a38489d0f670fb960f412/example/python/run.py
     wget https://fastapi.metacpan.org/source/MASH/Lingua-JA-Yomi-0.01/lib/Lingua/JA/bep-eng.dic

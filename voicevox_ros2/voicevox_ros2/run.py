29b18d39c347dbab771a688d0644ea6fa71a763f
GAI-313 <nakatogawagai@gmail.com>
2024-07-14 23:48:18 +0900

    CPU ver

--- voicevox_ros2/voicevox_ros2/run.py
+++ voicevox_ros2/voicevox_ros2/run.py
@@ -119,7 +119,7 @@ class Voicevox_ros2(Node):
             text = text.replace(" ", "")
 
             out = Path(generate_path)
-            acceleration_mode = AccelerationMode.AUTO
+            acceleration_mode = AccelerationMode.CPU
             core = VoicevoxCore(
                 acceleration_mode=acceleration_mode, open_jtalk_dict_dir=jtalk_path
             )
